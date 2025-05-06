089d4c7cd69c7e3cafb4bb863bf38962ec8013c5
rafiadpr <work.rafiadrianp@gmail.com>
2025-05-06 11:36:47 +0700

    [UPDATE] (QueueMain) : add menu and main logic for queue operation

---
+++ QueueMain23.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class QueueMain23 {
+    public static void menu() {
+        System.out.println("Masukkan operasi yang diinginkan : ");
+        System.out.println("1. Enqueue");
+        System.out.println("2. Dequeue");
+        System.out.println("3. Print");
+        System.out.println("4. Peak");
+        System.out.println("5. Clear");
+        System.out.println("-----------------------------------");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan kapasitas queue : ");
+        int n = sc.nextInt();
+
+        Queue23 Q = new Queue23(n);
+        int pilih;
+
+        do {
+            menu();
+            System.out.print("Pilih menu : ");
+            pilih = sc.nextInt();
+            switch (pilih) {
+                case 1:
+                    System.out.print("Masukkan data baru : ");
+                    int dataMasuk = sc.nextInt();
+                    Q.enqueue(dataMasuk);
+                    break;
+                case 2:
+                    int dataKeluar = Q.dequeue();
+                    if (dataKeluar != 0) {
+                        System.out.println("Data yang dikeluarkan : " + dataKeluar);
+                        break;
+                    }
+                case 3:
+                    Q.print();
+                    break;
+                case 4:
+                    Q.peek();
+                    break;
+                case 5:
+                    Q.clear();
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid");
+            }
+        } while (pilih != 0);
+    }
+}
